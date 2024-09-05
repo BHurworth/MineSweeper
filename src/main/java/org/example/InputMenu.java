8d5305491f7dc3c32b531abec3a1c74a2c562faa
unknown <ben.hurworth@gmail.com>
2024-09-05 07:57:12 +0100

    feat: remove boardManagement functionality from input menu

--- src/main/java/org/example/InputMenu.java
+++ src/main/java/org/example/InputMenu.java
@@ -6,41 +6,15 @@ import java.util.Scanner;
 public class InputMenu {
     BoardOperations boardOperations;
 
-    public boolean PlayerInputMenu(Board board, int[] revealedCellCount)
+    public static int PlayerInputMenu()
     {
-        if(boardOperations == null)
-        {
-            boardOperations = new BoardOperations(board.boardSettings);
-        }
-        board.displayBoard();
         Scanner scanner = new Scanner(System.in);
         System.out.println("\n\n\nEnter an input: " + "\n1. To reveal a square" + "\n2. To flag a block" + "\n3. Remove flag from a block" + "\n4. To Quit the game");
         while(!scanner.hasNextInt())
         {
             scanner.next();
         }
-        int choice = scanner.nextInt();
-
-       // return choice;
-        boolean gameOver = false;
-        switch(choice)
-        {
-            case 1:
-                gameOver = boardOperations.RemoveASquare(board.board, revealedCellCount);
-                break;
-            case 2:
-                boardOperations.FlagNewSquare(true, board.board);
-                break;
-            case 3:
-                boardOperations.FlagNewSquare(false, board.board);
-                break;
-            default:
-                System.exit(0);
-                break;
-        }
-
-        board.displayBoard();
-        return gameOver;
+        return scanner.nextInt();
     }
 
     public static BoardSettings BoardSettingsInputMenu()
